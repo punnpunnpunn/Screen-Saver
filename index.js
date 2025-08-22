33c5af8233fa7db0b62ce0db405815c470601e43
Punpun <punpunp@hotmail.com>
2025-08-22 15:42:37 +0700

    Made it so that you can add in multiple balls and randomize the color

--- index.js
+++ index.js
@@ -1,79 +1,100 @@
 class Ball {
-    constructor(x, y, r, dx, dy, canvas, ctx) {
+    constructor(x, y, r, dx, dy) {
         this.x = x;
         this.y = y;
         this.r = r;
         this.dx = dx;
         this.dy = dy;
-        this.canvas = canvas;
-        this.ctx = ctx;
+        this.red = randomNumber(255);
+        this.green = randomNumber(255);
+        this.blue = randomNumber(255);
     }
 
-    updatePosition() {
+    updatePosition(canvas) {
         this.x += this.dx;
         this.y += this.dy;
-        if (this.x > this.canvas.width) {
+        if (this.x > canvas.width) {
             this.dx = -Math.abs(this.dx);
         }
         else if (this.x < 0) {
             this.dx = Math.abs(this.dx);
         }
-        if (this.y > this.canvas.height) {
+        if (this.y > canvas.height) {
             this.dy = -Math.abs(this.dy);
         } else if (this.y < 0) {
             this.dy = Math.abs(this.dy);
         }
     }
 
-    drawBall() {
-        this.ctx.beginPath();
-        this.ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
-        this.ctx.fillStyle = "red";
-        this.ctx.fill();
-        this.ctx.stroke();
+    drawBall(ctx) {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+        ctx.fillStyle = `rgb(${this.red}, ${this.green}, ${this.blue})`;
+        ctx.fill();
+        ctx.stroke();
+    }
+}
+
+class Screen {
+    constructor(canvas, ballArr) {
+        this.canvas = canvas;
+        this.ballArr = ballArr;
+        this.ctx = canvas.getContext('2d');
+    }
+
+    updateCanvas() {
+        this.canvas.width = window.innerWidth * 0.8;
+        this.canvas.height = window.innerHeight * 0.8;
+    }
+
+    updateBalls() {
+        this.ballArr.forEach(b => b.updatePosition(this.canvas));
+    }
+
+    clear() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    draw() {
+        this.ballArr.forEach(b => b.drawBall(this.ctx));
+    }
+
+    addBall() {
+        let speed = 8;
+        let direction = randomNumber(2 * Math.PI);
+        let dx = speed * Math.cos(direction);
+        let dy = speed * Math.sin(direction);
+        this.ballArr.push(new Ball(randomNumber(canvas.width), randomNumber(canvas.height), 10, dx, dy))
     }
 }
 
 const animationLoop = () => {
-    canvas.width = window.innerWidth * 0.8;
-    canvas.height = window.innerHeight * 0.8;
+    screen.updateCanvas();
     if (play) {
-        update();
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        screen.updateBalls();
+        screen.clear();
     }
-    drawScreen();
+    screen.draw();
     window.requestAnimationFrame(animationLoop);
-
 }
+
 const toggleAnimation = () => {
     play = !play;
-}
-
-const update = () => {
-    ball.updatePosition();
-}
-
-const drawScreen = () => {
-    ball.drawBall();
 }
 
 const randomNumber = (n) => {
     return Math.floor(Math.random() * n) + 1;
 }
 
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth * 0.8;
-canvas.height = window.innerHeight * 0.8;
+const setUp = () => {
+    screen.addBall()
+    play = true;
+}
 
-let speed = 8;
-let direction = randomNumber(2 * Math.PI);
-let dx = speed * Math.cos(direction);
-let dy = speed * Math.sin(direction);
-let ball = new Ball(randomNumber(canvas.width), randomNumber(canvas.height), 10, dx, dy, canvas, ctx);
-let play = true;
-let req;
+let play;
+let screen = new Screen(document.getElementById('canvas'), []);
 
 window.onload = () => {
-    req = window.requestAnimationFrame(animationLoop);
+    setUp();
+    window.requestAnimationFrame(animationLoop);
 }
