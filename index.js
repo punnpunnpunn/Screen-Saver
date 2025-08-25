ec328ade845f00b9140a88a08758c3f8d43f7bfc
Punpun <punpunp@hotmail.com>
2025-08-26 01:00:55 +0700

    Ball collision logic done (WITH PHYSICS!!!)

--- index.js
+++ index.js
@@ -33,6 +33,10 @@ class Ball {
         ctx.fill();
         ctx.stroke();
     }
+
+    detectCollision(ball) {
+        return (Math.hypot(this.x-ball.x, this.y-ball.y) < this.r + ball.r)
+    }
 }
 
 class Screen {
@@ -66,12 +70,58 @@ class Screen {
         let dy = speed * Math.sin(direction);
         this.ballArr.push(new Ball(randomNumber(canvas.width), randomNumber(canvas.height), 10, dx, dy))
     }
+
+    checkCollision() {
+        for (let b1 in this.ballArr) {
+            b1 = Number(b1)
+            for (let b2 in this.ballArr.slice(Number(b1) + 1)) {
+                b2 = Number(b2)
+                if (this.ballArr[b2 + Number(b1) + 1] && this.ballArr[b1].detectCollision(this.ballArr[b2 + b1 + 1])) {
+                    this.resolveCollision(this.ballArr[b1], this.ballArr[b2 + b1 + 1])
+                    console.log("collision")
+                }
+            }
+        }
+    }
+
+    resolveCollision(b1, b2) {
+        let theta1, theta2, phi, v1, v2;
+        theta1 = findAngle(b1.dx, b1.dy);
+        theta2 = findAngle(b2.dx, b2.dy);
+        phi = findAngle(b1.x - b2.x, b1.y - b2.y);
+        v1 = Math.hypot(b1.dx, b1.dy)
+        v2 = Math.hypot(b2.dx, b2.dy)
+        b1.dx = v2*Math.cos(theta2 - phi) * Math.cos(phi) + v1 * Math.sin(theta1 - phi) * Math.cos(phi + Math.PI/2)
+        b1.dy = v2*Math.cos(theta2 - phi) * Math.sin(phi) + v1 * Math.sin(theta1 - phi) * Math.sin(phi + Math.PI/2)
+        b2.dx = v1*Math.cos(theta1 - phi) * Math.cos(phi) + v2 * Math.sin(theta2 - phi) * Math.cos(phi + Math.PI/2)
+        b2.dy = v1*Math.cos(theta1 - phi) * Math.sin(phi) + v2 * Math.sin(theta2 - phi) * Math.sin(phi + Math.PI/2)
+        
+        function findAngle(x, y) {
+            let theta
+            if (x) {
+                theta = Math.atan(y / x);
+                if (x < 0) {
+                    theta = Math.PI - theta;
+                }
+            }
+            else {
+                if (y > 0) {
+                    theta = Math.PI / 2;
+                }
+                else if (y < 0) {
+                    theta = -Math.PI / 2;
+                }
+            }
+            return theta;
+        }
+    }
 }
 
 const animationLoop = () => {
     screen.updateCanvas();
     if (play) {
         screen.updateBalls();
+        screen.checkCollision();
         screen.clear();
     }
     screen.draw();
